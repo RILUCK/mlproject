50f9ed2e016edb9588e136edc700c5dd0f339cff
Rishabh <rishabhshrivas22@gmail.com>
2023-03-10 23:25:50 +0100

    Requirement fix and exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exe_tb=error_detail.exc_info()
@@ -23,5 +23,5 @@ class CustomException(Exception):
 #     try:
 #         a = 1/0
 #     except Exception as e:
-#         # logging.info("Devide by zero")
+#         logging.info("Devide by zero")
 #         raise CustomException(e, sys)
