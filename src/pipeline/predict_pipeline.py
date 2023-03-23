70bc5aa192e176652ec2cbd6168356e346b7dd28
Rishabh <rishabhshrivas22@gmail.com>
2023-03-23 11:28:00 +0100

    Deployment configuration

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -32,17 +32,11 @@ class CustomData:
         writing_score: int):
 
         self.gender = gender
-
         self.race_ethnicity = race_ethnicity
-
         self.parental_level_of_education = parental_level_of_education
-
         self.lunch = lunch
-
         self.test_preparation_course = test_preparation_course
-
         self.reading_score = reading_score
-
         self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
